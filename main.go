3f6228637e31c028c261e1d3a0817084db8d40e3
German Daniel Boso <german.boso@gmail.com>
2021-03-07 16:05:58 -0300

    seccion 4 video 15 de curso go
    completado el video 15 del curso de Go

--- main.go
+++ main.go
@@ -195,9 +195,26 @@ func bucles (){
 }
 
 func operacionesConStrings(){
-	var texto = "hola go, hola german, hola mundo"
+	var texto = "Hola go, Hola german, Hola mundo"
+
+	var texto2 = "Cadena 2"
 
 	fmt.Println(texto)
 
 	fmt.Println(strings.ToUpper(texto))
+
+	fmt.Println(strings.ToLower(texto))
+
+	fmt.Println(strings.Replace(texto, "Hola", "Adios", -1))
+
+	fmt.Println(strings.Compare(texto, texto2))
+
+	fmt.Println(strings.Split(texto, ","))
+
+	fmt.Println(strings.Contains(texto, "Hola"))
+
+	fmt.Println(strings.Contains(texto, "coche"))
+
+
+
 }
